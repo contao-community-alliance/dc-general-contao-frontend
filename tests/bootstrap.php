14019bd00e2aae2ce04df79b824d654543b08767
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-11-09 19:47:02 +0100

    Setup project base

---
+++ tests/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dc-general-contao-frontend.
+ *
+ * (c) 2013-2015 Contao Community Alliance.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dc-general-contao-frontend
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2013-2015 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general-contao-frontend/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+    return file_exists($file) ? include $file : false;
+}
+
+if (
+    // Locally installed dependencies
+    (!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
+    // We are within an composer install.
+    && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL;
+    exit(1);
+}
