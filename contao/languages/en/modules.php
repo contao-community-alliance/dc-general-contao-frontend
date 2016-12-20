03697f8fa1c78b5ee5fab554449c7b92e0974390
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-12-20 14:34:14 +0100

    Add language file

---
+++ contao/languages/en/modules.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dc-general-contao-frontend.
+ *
+ * (c) 2016 Contao Community Alliance.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dc-general-contao-frontend
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2016 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general-contao-frontend/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['MOD']['dc-general-contao-frontend'][0] = 'DC-General frontend editing';
+$GLOBALS['TL_LANG']['MOD']['dc-general-contao-frontend'][1] = 'Provide frontend editing via dc-general';
+
+$GLOBALS['TL_LANG']['CTE']['dc-general-contao-frontend'][0] = 'DC-General frontend editing';
+$GLOBALS['TL_LANG']['CTE']['dc-general-contao-frontend'][1] = 'Provide frontend editing via dc-general';
