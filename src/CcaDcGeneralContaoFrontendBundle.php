13c2a58cdfc029928077c201539dd06010455ea3
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-10-31 23:36:16 +0100

    Transform to Contao Bundle

--- src/CcaDcGeneralContaoFrontendBundle.php
+++ src/CcaDcGeneralContaoFrontendBundle.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of contao-community-alliance/dc-general-contao-frontend.
  *
- * (c) 2015 Contao Community Alliance.
+ * (c) 2015-2017 Contao Community Alliance.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,17 +11,21 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    contao-community-alliance/dc-general-contao-frontend
- * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2015 Contao Community Alliance.
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright  2015-2017 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/dc-general-contao-frontend/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
+namespace ContaoCommunityAlliance\DcGeneral\ContaoFrontend;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
 /**
- * Register the templates
+ * The Bundle class.
  */
-TemplateLoader::addFiles(
-    [
-        'dcfe_general_edit'   => 'system/modules/dc-general-contao-frontend/templates',
-    ]
-);
+class CcaDcGeneralContaoFrontendBundle extends Bundle
+{
+
+}
---
+++ src/DependencyInjection/CccDcGeneralContaoFrontendExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/dc-general-contao-frontend.
+ *
+ * (c) 2015-2017 Contao Community Alliance.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contao-community-alliance/dc-general-contao-frontend
+ * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright  2015-2017 Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/dc-general-contao-frontend/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\DcGeneral\ContaoFrontend\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * This is the Bundle extension.
+ */
+class CcaDcGeneralContaoFrontendExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('listeners.yml');
+    }
+}
