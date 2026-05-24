efec8afc0c249284e18355b0c6c1503596f8993e
Ingolf Steinhardt <info@e-spin.de>
2026-05-24 14:05:46 +0200

    Fix to show fallback language label
    (cherry picked from commit 797a751e63d64a20745801f08fdf736c52e1a6f1)

--- src/View/EditMask.php
+++ src/View/EditMask.php
@@ -625,6 +625,7 @@ class EditMask
             $template
                 ->set('languages', $controller->getSupportedLanguages($this->model->getId()))
                 ->set('language', $dataProvider->getCurrentLanguage())
+                ->set('fallbackLanguage', $dataProvider->getFallbackLanguage($this->model->getId())?->getLocale())
                 ->set('languageSubmit', $translator->translate('change-language', 'dc-general'))
                 ->set('languageLabel', $translator->translate('language', 'dc-general'))
                 ->set('languageHeadline', $languages[$dataProvider->getCurrentLanguage()] ?? '');
@@ -634,6 +635,7 @@ class EditMask
 
         $template
             ->set('languages', null)
+            ->set('fallbackLanguage', null)
             ->set('languageHeadline', '');
     }
 }
